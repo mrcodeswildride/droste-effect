d06b1b74fa7d3bde76fb85c1e3b3a2cf73848b0d
David Keller <mrcodeswildride@gmail.com>
2021-03-10 19:39:48 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# droste-effect
+
+Droste effect web app written in HTML, CSS, and JavaScript.
+
+Use this app at [drosteeffect.mrcodeswildride.com](https://drosteeffect.mrcodeswildride.com/).
+Learn how to make this app at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
---
+++ script.js
@@ -0,0 +1,44 @@
+let width = document.getElementById(`width`)
+let mirrorButton = document.getElementById(`mirrorButton`)
+let box = document.getElementById(`box`)
+
+mirrorButton.addEventListener(`click`, showMirror)
+
+width.addEventListener(`keydown`, keyPressed)
+width.focus()
+
+function showMirror() {
+  let widthValue = width.value.trim()
+
+  if (widthValue != `` && !isNaN(widthValue)) {
+    if (widthValue > 900) {
+      box.innerHTML = `Mirror width cannot be more than 900.`
+    }
+    else {
+      box.innerHTML = ``
+
+      let marginValue = 0
+      let parentBox = box
+
+      while (widthValue >= 1) {
+        let childBox = document.createElement(`div`)
+        childBox.style.width = `${widthValue}px`
+        childBox.style.height = `${widthValue}px`
+        childBox.style.backgroundSize = `${widthValue}px ${widthValue}px`
+        childBox.style.marginTop = `${marginValue}px`
+
+        parentBox.appendChild(childBox)
+
+        widthValue = widthValue * 2/3
+        marginValue = widthValue / 4
+        parentBox = childBox
+      }
+    }
+  }
+}
+
+function keyPressed(event) {
+  if (event.keyCode == 13) {
+    showMirror()
+  }
+}
